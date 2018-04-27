d6933b978b68ae40e42f139edb601bfd8d35770c
bflores4cd <bflores4@ucol.mx>
2018-04-27 09:57:52 -0500

    practica graficos tortuga

---
+++ práctica gráficos de tortuga/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace práctica_gráficos_de_tortuga
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Lucida Console", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(84, 221);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(362, 269);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 106);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 582);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 8, 4, 8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ práctica gráficos de tortuga/tortuga.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace práctica_gráficos_de_tortuga
+{
+    class Tortuga
+    {
+        private bool _estadoLapiz; //lapiz arriba = false, abajo = true
+        private int _enColumna;
+        private int _enRenglon;
+        private int _orientacion;
+
+        public bool estadoLapiz { get { return _estadoLapiz; } }
+        public int enColumna { get { return _enColumna; } }
+        public int enRenglon { get { return _enRenglon; } }
+        public int orientacion { get { return _orientacion; } } //0,1,2,3
+
+        public Tortuga()
+        {
+            _enColumna = 0;
+            _enRenglon = 0;
+            _estadoLapiz = false;
+            _orientacion = 1;
+        }
+
+        public bool cambiarEstadoLapiz()
+        {
+            return _estadoLapiz = !_estadoLapiz;
+        }
+
+        public int girarDerecha()
+        {
+            if (_orientacion == 3)
+                return _orientacion = 0;
+            else
+                return _orientacion++;
+        }
+
+        public int girarIzquierda()
+        {
+            if (_orientacion == 0)
+                return _orientacion = 3;
+            else
+                return _orientacion--;
+        } 
+
+        private bool avanzarCasilla()
+        {
+            bool posibleAvanzar;
+
+            if (_enRenglon == 0 && _orientacion == 0 ||
+                _enColumna == 0 && _orientacion == 3 ||
+                _enRenglon == 19 && _orientacion == 2 ||
+                _enColumna == 19 && _orientacion == 1 )
+                posibleAvanzar = false;
+            else
+
+            switch (_orientacion)
+            {
+                case 0: _enRenglon++;
+                    break;
+                case 1: _enColumna
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+            }
+
+            return posibleAvanzar;
+        }
+
+        public void avanzarVariasCasillas(int numCasillas)
+        {
+            
+        } 
+    }
+}
